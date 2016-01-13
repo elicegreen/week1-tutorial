9909147e4aa5ae1c94d3e0ccd34a74690ba4f888
Elice Greenberg <egreenberg3@mail.valenciacollege.edu>
2016-01-12 21:28:08 -0500

    Steps 10 and 11 of instructions #2

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is Elice'
+	return 'hello world, my name is '
 
-print( hello_world() + 'test student' )
+print( hello_world() + 'Elice' )
